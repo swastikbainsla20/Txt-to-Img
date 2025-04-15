313a57935da5913ab81bf8e8a82fd21597cd02ec
swastikbainsla20 <75483141+swastikbainsla20@users.noreply.github.com>
2025-04-16 00:37:07 +0530

    Add files via upload

--- imagegenflask/Txt-to-Img/imagegenflask/app.py
+++ imagegenflask/Txt-to-Img/imagegenflask/app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask import render_template
-import openai
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,10 +9,6 @@ def home():
 @app.route("/About")
 def about():
     return render_template("about.html")
-
-@app.route("/generateimages")
-def index():
-    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ imagegenflask/tryme.py
@@ -0,0 +1,14 @@
+import os
+from openai import OpenAI
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+response = client.images.generate(
+    model="dall-e-3",
+    prompt="a white siamese cat",
+    size="1024x1024",
+    quality="standard",
+    n=1,
+)
+
+print(response.data[0].url)
